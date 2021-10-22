466af1a980f4cf0ee81b0a35644ef2920b3c98f6
Harry <haesoo9410@gmail.com>
2021-10-23 00:03:43 +0900

    Update 55

--- dfs/55.js
+++ dfs/55.js
@@ -1,21 +1,15 @@
-// 시간초과
+// 그리디
 
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 var canJump = function (nums) {
-  return dfs(0, nums);
-};
-
-function dfs(index, arr) {
-  const target = arr.length - 1;
-  if (index === target) return true;
-  if (index > target) return false;
-  const maxJump = arr[index];
-  for (let i = 1; i <= maxJump; i++) {
-    const isReach = dfs(index + i, arr);
-    if (isReach) return true;
+  const target = nums.length - 1;
+  let max = 0;
+  for (let i = 0; i <= target; i++) {
+    max = Math.max(max, i + nums[i]); // 현재 최대로 갈 수 있는 범위
+    if (max >= target) return true;
+    if (max === i) return false;
   }
-  return false;
-}
+};
